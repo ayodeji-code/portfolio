c84bd7a4deee814fe3afa23f7a84de3845d0bd21
Azeez Abdulrahman <105288639+ayodeji-code@users.noreply.github.com>
2023-12-05 16:33:54 +0000

    Pending changes exported from your codespace

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -2,12 +2,32 @@ import React from "react";
 
 
 const Education = () => {
+    const sectionStyle = {
+        width: "700px" ,
+        margin: "auto",
+        alignItems: "center",
+        height: "200px"
+    };
+    const gridContainerStyle = {
+        display: "grid",
+        gap: "10px"
+    };
+
+    const gridItemStyle = {
+        padding: "17px"
+    };
+    const educationHeader = {
+        textAlign: "center"    
+    };
     return(
-        <section className="light" id="education">
-            <h2>Education</h2>
+        <section className="light" id="education" style={sectionStyle}>
+            <h2 id="educationHeader" style={educationHeader}>Education</h2>
+            <div className="grid-container" style={gridContainerStyle}>
+                <div className="grid-item" style={gridItemStyle}>Federal University of Technology, Minna, Niger State, Nigeria</div>
+            </div>
         </section>
     )
 }
 
 export default Education;
-/*add grid of 4 cells */
+
