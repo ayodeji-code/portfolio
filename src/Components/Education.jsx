523437c9239d8a39a21b84e55342e7c448feeb42
Azeez Abdulrahman <105288639+ayodeji-code@users.noreply.github.com>
2023-12-05 13:23:25 +0000

    Pending changes exported from your codespace

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -2,12 +2,24 @@ import React from "react";
 
 
 const Education = () => {
+    const gridContainerStyle = {
+        display: 'grid',
+        gap: '10px'
+    };
+
+    const gridItemStyle = {
+        border: '1px solid black',
+        padding: '10px'
+    };
     return(
         <section className="light" id="education">
             <h2>Education</h2>
+            <div className="grid-container" style={gridContainerStyle}>
+                <div className="grid-item" style={gridItemStyle}>Federal UNiversity of Technology, Minna.</div>
+            </div>
         </section>
     )
 }
 
 export default Education;
-/*add grid of 4 cells */
+/*create grid of 2 cells*/
